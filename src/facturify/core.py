6ae8eb113f40036bb8613036bdfca94fd88a268c
Isaac Sánchez <1031819+isaacasancheza@users.noreply.github.com>
2025-03-25 14:53:27 -0600

    :recycle: use requests exceptions instead of own and add retry logic

--- src/facturify/core.py
+++ src/facturify/core.py
@@ -1,9 +1,8 @@
 from abc import ABC
-from typing import Any, Literal, NotRequired, TypedDict, Unpack
+from time import sleep
+from typing import Any, Callable, Literal, NotRequired, TypedDict, Unpack
 
 import requests
-
-from facturify import exceptions
 
 type URL = str | int
 type Body = dict
@@ -11,6 +10,7 @@ type Params = dict
 type Method = Literal['GET', 'PUT', 'POST', 'PATCH', 'DELETE']
 type Headers = dict
 type ApiVersion = Literal['v1']
+type ApiToken = str | Callable[[], str]
 
 SANDBOX_URL = 'https://api-sandbox.facturify.com/api'
 PRODUCTION_URL = 'https://api.facturify.com/api'
@@ -25,14 +25,18 @@ class OptionalArguments(TypedDict):
 class Core(ABC):
     def __init__(
         self,
-        access_token: str,
+        api_token: ApiToken,
         /,
         *,
         version: ApiVersion = 'v1',
         sandbox: bool = False,
+        max_retries: int = 3,
+        retriable_http_codes: set[int] = {401 | 429 | 500 | 502 | 503 | 504},
     ) -> None:
         self._api_root = (SANDBOX_URL if sandbox else PRODUCTION_URL) + f'/{version}/'
-        self._access_token = access_token
+        self._api_token = api_token
+        self._max_retries = max_retries
+        self._retriable_http_codes = retriable_http_codes
 
     def get(
         self,
@@ -40,7 +44,7 @@ class Core(ABC):
         *args: URL,
         **kwargs: Unpack[OptionalArguments],
     ):
-        return self.request(
+        return self.wrapper(
             *args,
             **kwargs,
             method='GET',
@@ -53,7 +57,7 @@ class Core(ABC):
         *args: URL,
         **kwargs: Unpack[OptionalArguments],
     ):
-        return self.request(
+        return self.wrapper(
             *args,
             **kwargs,
             body=body,
@@ -67,7 +71,7 @@ class Core(ABC):
         *args: URL,
         **kwargs: Unpack[OptionalArguments],
     ):
-        return self.request(
+        return self.wrapper(
             *args,
             **kwargs,
             body=body,
@@ -81,7 +85,7 @@ class Core(ABC):
         *args: URL,
         **kwargs: Unpack[OptionalArguments],
     ):
-        return self.request(
+        return self.wrapper(
             *args,
             **kwargs,
             body=body,
@@ -95,12 +99,45 @@ class Core(ABC):
         *args: URL,
         **kwargs: Unpack[OptionalArguments],
     ):
-        return self.request(
+        return self.wrapper(
             *args,
             **kwargs,
             body=data,
             method='DELETE',
         )
+
+    def wrapper(
+        self,
+        *args: URL,
+        body: Body | None = None,
+        params: Params | None = None,
+        method: Method,
+        headers: Headers | None = None,
+        ignore_auth: bool | None = False,
+    ) -> dict[str, Any]:
+        retries = 0
+        exception = None
+        while retries < self._max_retries:
+            try:
+                return self.request(
+                    *args,
+                    body=body,
+                    params=params,
+                    method=method,
+                    headers=headers,
+                    ignore_auth=ignore_auth,
+                )
+            except requests.HTTPError as e:
+                exception = e
+                if e.response.status_code in self._retriable_http_codes:
+                    backoff = min(2**retries, 20)
+                    sleep(backoff)
+                    retries += 1
+                    continue
+                raise
+        if exception:
+            raise exception
+        raise RuntimeError('This code should not be reached')
 
     def request(
         self,
@@ -114,7 +151,12 @@ class Core(ABC):
         if not ignore_auth:
             if not headers:
                 headers = {}
-            headers['Authorization'] = f'Bearer {self._access_token}'
+            api_token = (
+                self._api_token
+                if isinstance(self._api_token, str)
+                else self._api_token()
+            )
+            headers['Authorization'] = f'Bearer {api_token}'
 
         url = self._api_root + '/'.join(str(arg) for arg in args)
 
@@ -126,18 +168,6 @@ class Core(ABC):
             headers=headers,
         )
 
-        data = response.json()
-
-        if response.ok:
-            return data
-
-        match response.status_code:
-            case 401:
-                raise exceptions.UnauthorizerdError(data)
-            case 404:
-                raise exceptions.NotFoundError(data)
-            case 422:
-                raise exceptions.UnprocessableEntityError(data)
-
         response.raise_for_status()
-        raise
+
+        return response.json()
